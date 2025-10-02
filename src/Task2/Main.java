e4890b656ea82881e756e3025169c699b72e4cae
MikkelNorden <mikkelhoegnorden@gmail.com>
2025-10-02 16:05:01 +0200

    Task2 finish

--- src/Task2/Main.java
+++ src/Task2/Main.java
@@ -2,6 +2,10 @@ package Task2;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        Cafe cafe = new Cafe();
+        cafe.loadMenuData();
+        for(String coffee : cafe.getCoffeeMenu()) {
+            System.out.println(cafe.getCoffeeMenu());
+        }
     }
 }
