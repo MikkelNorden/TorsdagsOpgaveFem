5ffe97233ded5150cf08061ab5d4f06a406ec660
MikkelNorden <mikkelhoegnorden@gmail.com>
2025-10-02 15:28:37 +0200

    Task1 complete

--- src/Task1/Main.java
+++ src/Task1/Main.java
@@ -4,10 +4,13 @@ import java.util.ArrayList;
 
 public class Main {
     public static void main(String[] args) {
-        ArrayList<Customer> customers = new ArrayList<>();
+
         Customer customer1 = new Customer("John", "Johnson", "john0210");
         Customer customer2 = new Customer("Jack", "Jackson", "jack0210");
         Customer customer3 = new Customer("Jock", "Jockson", "jock0210");
+
+        ArrayList<Customer> customers = new ArrayList<>();
+
         customers.add(customer1);
         customers.add(customer2);
         customers.add(customer3);
@@ -15,8 +18,8 @@ public class Main {
         printCustomers(customers);
     }
 
-    static void printCustomers(ArrayList customers){
-        for(Object customer: customers){
+    public static void printCustomers(ArrayList<Customer> customers){
+        for(Customer customer: customers){
             System.out.println(customer);
         }
     }
